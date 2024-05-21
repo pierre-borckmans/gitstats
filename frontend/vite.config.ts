5ce88fb4c083fa3d132b01edab4e193b8fe05115
Pierre Borckmans <pborckmans@gmail.com>
2024-05-21 09:15:35 +0200

    React 19

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 // if in ESM context
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const ReactCompilerConfig = {
+  sources: (filename) => {
+    return true;
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
-  }
-})
+    },
+  },
+});
